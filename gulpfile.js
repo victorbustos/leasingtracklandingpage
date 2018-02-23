bf3fc60f9605a5688b958ac37877bc9263c1b653
victorbustos <victor@creative-effect.com>
2018-02-22 19:40:13 -0500

    finshed layout

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,43 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    browserSync = require('browser-sync').create();
 
 
 // Define file sources
 var sassMain = ['development/sass/leadgen.scss'];
 var sassSources = ['development/sass/**/*.scss']; // Any .scss file in any sub-directory
 var jsSources = ['development/scripts/*.js']; // Any .js file in scripts directory
+
+
+var browserSyncWatchFiles = [
+    'css/*.css',
+    'js/*.js',
+    '*.html'
+];
+
+// browser-sync options
+// see: https://www.browsersync.io/docs/options/
+var browserSyncOptions = {
+    // proxy: "http://localhost:8000",
+    // xip: true,
+    server: {
+      baseDir: "./"
+    },
+    notify: {
+      styles: {
+        fontSize: '10px', position: 'fixed',
+        top: '0', left: '43%', width: '200px',
+        margin: '0px', padding: '5px',
+        zIndex: '9999', display: 'block',
+        borderRadius: '10px',
+        color: 'white', textAlign: 'center',
+        backgroundColor: 'rgba(55, 53, 47, 0.98)'
+      }
+    },
+};
+
 
 
 // Task to compile SASS files
@@ -39,7 +69,15 @@ gulp.task('watch', function() {
     gulp.watch(jsSources,['concat']); 
 });
 
+// Run:
+// gulp browser-sync
+// Starts browser-sync task for starting the server.
+gulp.task('browser-sync', function() {
+    browserSync.init(browserSyncWatchFiles, browserSyncOptions );
+});
+
+
 
 // Default gulp task
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'watch','browser-sync']);
 // gulp.task('default', ['sass', 'concat', 'watch']);
---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+<link rel="stylesheet" href="https://leasingtrack.com/new/css/bootstrap.min.css">
+<link rel="stylesheet" href="/css/leadgen.css">
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+<script src="js/leadgen-scripts.js" charset="utf-8"></script>
+</head>
+<body>
+  
+  <div class="container-fluid lead-gen">
+    <div class="row">
+      <div class="">
+        
+        <div class="col-md-4 left-green-wrap">
+          <div class="clearfix"></div>
+          <h1><span> <span class="phone-break-tiny"> Are You Still</span> Saying No</span> to customers that can’t afford cash, have bad credit or you simply don’t want to deal with financing?</h1>
+          <img class="img-responsive center-block" src="images/credit-rejected.png" alt="No more unnapproved credit applications">
+          <div class="logo visible-xs visible-sm">
+            <img class="img-responsive center-block" src="images/leasing-track-logo.png" alt="Leasing Track logo">
+          </div>
+          <div class="fullheight-fill"></div>
+        </div>
+        
+        <div class="col-md-8">
+          <div class="row">
+            <div class="col-lg-12 top-blue-wrap">
+              <div class="col-lg-7 col-sm-6">
+                <p class="lead">Let us do the financing for you and approve more customers through us.</p>
+              </div>
+              <div class="col-lg-5 col-sm-6 call-wrap">
+                <a class="btn btn-primary btn-block btn-lg" href="tel:855-865-6046"><span class="phone-break">CALL US:</span> (855) 865-6046</a>
+              </div>
+            </div>
+            
+            <div class="col-lg-12">
+              <div class="row reasons-wrap">
+                <div class="col-lg-7">
+                  <h2><span class="seven">7</span><span class="reasons">Reasons to</span> Let Us Be Your Finacing Source</h2>    
+                  
+                  <div class="col-lg-2 col-md-2 col-sm-1 col-xs-2">
+                     <div class="num">1</div>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8">
+                    <h3>Ease of Use</h3>
+                    <p>Our finance portal is so simple to use, it is 100% fully automated for you and reaches out to your customer automatically so everyone knows the status at all times.</p>
+                  </div>
+                  <div class="clearfix"></div>
+                  
+                  <div class="col-lg-2 col-md-2 col-sm-1 col-xs-2">
+                     <div class="num">2</div>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8">
+                    <h3>95% Approval Rate</h3>
+                    <p>We have 25 banks competing for your clients business and 95% of the time we get applicants approved!</p>
+                  </div>
+                  <div class="clearfix"></div>
+                  
+                  <div class="col-lg-2 col-md-2 col-sm-1 col-xs-2">
+                     <div class="num">3</div>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8">
+                    <h3>Leasing is Profitable</h3>
+                    <p>The margin is higher on leased equipment.</p>
+                  </div>
+                  <div class="clearfix"></div>
+                  
+                  <div class="col-lg-2 col-md-2 col-sm-1 col-xs-2">
+                     <div class="num">4</div>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8">
+                    <h3>Tax Savings</h3>
+                    <p>Your client can save thousands in taxes since Lease-to-own programs are qualified for a 100% tax write off!</p>
+                  </div>
+                  <div class="clearfix"></div>
+                  
+                  <div class="col-lg-2 col-md-2 col-sm-1 col-xs-2">
+                     <div class="num">5</div>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8">
+                    <h3>Conserve More Capital</h3>
+                    <p>This will allow your customers to save capital and use it for their business.</p>
+                  </div>
+                  <div class="clearfix"></div>
+                  
+                  <div class="col-lg-2 col-md-2 col-sm-1 col-xs-2">
+                     <div class="num">6</div>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8">
+                    <h3>Appealing to Customers</h3>
+                    <p>You will have more repeat customers because you’re making the equipment affordable.</p>
+                  </div>
+                  <div class="clearfix"></div>
+                  
+                  <div class="col-lg-2 col-md-2 col-sm-1 col-xs-2">
+                     <div class="num">7</div>
+                  </div>
+                  <div class="col-lg-10 col-md-10 col-sm-8">
+                    <h3>Our Expertise</h3>
+                    <p>We are experts in the food service equipment industry and can handle all of the financing issues for you.</p>
+                  </div>
+                  <div class="clearfix"></div>
+                  
+                  <div class="cta-area">
+                    <p class="letus">Let us do the financial negotiating for you and stand out to your customers!</p>
+                    <div class="text-center">
+                      <a class="btn btn-primary btn-block btn-lg" href="#">Become a Dealer</a>  
+                    </div>
+                    <p class="questions">Questions? Give Us a Call:<br>
+                      <span>(855) 865-6046</span>
+                    </p>
+                  </div>
+                  
+                </div>
+                
+                
+                <div class="col-lg-5 last-col">
+                  <img class="img-responsive center-block hidden-sm hidden-xs" src="images/dishwashers.png" alt="Commercial Dishwashers">
+                  <a class="" href="#">
+                    <img class="img-responsive center-block top-gap" src="images/leasing-track-logo.png" alt="Leasing Track logo">
+                  </a>
+                  <p class="we-are">We are the equipment industry leader in tailored financing programs. We also pride ourselves in being the fastest online approval system in the industry.</p>
+                  <div class="contact-form">
+                    <div class="form-area bottom-gap">  
+                      <form role="form">
+                        <h3 class="text-center">Contact Form</h3>
+                  			<div class="form-group">
+              						<input type="text" class="form-control input-lg" id="name" name="name" placeholder="Name" required>
+              					</div>
+              					<div class="form-group">
+              						<input type="text" class="form-control input-lg" id="email" name="email" placeholder="Email" required>
+              					</div>
+                        <div class="form-group">
+                          <textarea class="form-control input-lg" type="textarea" id="message" placeholder="Message" maxlength="140" rows="7"></textarea>
+                        </div>
+                        <button type="button" id="submit" name="submit" class="btn btn-primary btn-lg btn-block">Send Message</button>
+                      </form>
+                    </div>                    
+                    
+                  </div>
+                  <img class="img-responsive center-block top-gap" src="images/restaurant-equipment.png" alt="Restaurant equipment">
+                </div>
+                
+                
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <a href="#">
+                <img class="img-responsive center-block" src="images/leasing-track-logo.png" alt="Leasing Track logo">
+              </a>
+            </div>
+          </div>
+        </div>
+        
+        <div class="footer">
+          
+        </div>
+      </div> 
+    </div>
+  </div>
+
+</body>
+</html>
